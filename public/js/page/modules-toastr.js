6a1ab81862fd6285ccef0eb404a21839422c5663
Reza Fahlevi <rezafahlevi55@gmail.com>
2024-07-02 08:31:37 +0700

    fix modules-sweetalert js

--- public/js/page/modules-toastr.js
+++ public/js/page/modules-toastr.js
@@ -1,6 +1,6 @@
 "use strict";
 
-$("#toastr-1").click(function() {
+$("#toastr-1").on('click', function () {
   iziToast.info({
     title: 'Hello, world!',
     message: 'This awesome plugin is made iziToast toastr',
@@ -8,7 +8,7 @@ $("#toastr-1").click(function() {
   });
 });
 
-$("#toastr-2").click(function() {
+$("#toastr-2").on('click', function () {
   iziToast.success({
     title: 'Hello, world!',
     message: 'This awesome plugin is made by iziToast',
@@ -16,7 +16,7 @@ $("#toastr-2").click(function() {
   });
 });
 
-$("#toastr-3").click(function() {
+$("#toastr-3").on('click', function () {
   iziToast.warning({
     title: 'Hello, world!',
     message: 'This awesome plugin is made by iziToast',
@@ -24,7 +24,7 @@ $("#toastr-3").click(function() {
   });
 });
 
-$("#toastr-4").click(function() {
+$("#toastr-4").on('click', function () {
   iziToast.error({
     title: 'Hello, world!',
     message: 'This awesome plugin is made by iziToast',
@@ -32,34 +32,34 @@ $("#toastr-4").click(function() {
   });
 });
 
-$("#toastr-5").click(function() {
+$("#toastr-5").on('click', function () {
   iziToast.show({
     title: 'Hello, world!',
     message: 'This awesome plugin is made by iziToast',
-    position: 'bottomRight' 
+    position: 'bottomRight'
   });
 });
 
-$("#toastr-6").click(function() {
+$("#toastr-6").on('click', function () {
   iziToast.show({
     title: 'Hello, world!',
     message: 'This awesome plugin is made by iziToast',
-    position: 'bottomCenter' 
+    position: 'bottomCenter'
   });
 });
 
-$("#toastr-7").click(function() {
+$("#toastr-7").on('click', function () {
   iziToast.show({
     title: 'Hello, world!',
     message: 'This awesome plugin is made by iziToast',
-    position: 'bottomLeft' 
+    position: 'bottomLeft'
   });
 });
 
-$("#toastr-8").click(function() {
+$("#toastr-8").on('click', function () {
   iziToast.show({
     title: 'Hello, world!',
     message: 'This awesome plugin is made by iziToast',
-    position: 'topCenter' 
+    position: 'topCenter'
   });
 });
